e0f99385486e51c12ab157969530bc73b1508e8a
Seth <seth.tales@gmail.com>
2018-07-29 14:53:30 -0700

    corrected typo in app.js, removed routes folder

--- app.js
+++ app.js
@@ -3,9 +3,4 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
-
-app.get('/', (req, res) => res.sendFile(__dirname + '/views/GardenView.html'))
-
-
-
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
