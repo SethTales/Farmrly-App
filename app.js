4741f24ccb3d47a2865c38b480f269769be0f8d2
Seth <seth.tales@gmail.com>
2018-07-30 07:17:47 -0700

    Fixed routing bug in app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-
 const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/views/GardenView.html'));
